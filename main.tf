7341606ceef3398dd17b70987a1d8e249758f341
Joseph Dizon <joseph.dizon@ctsolutions.com.au>
2023-09-11 16:18:34 +1000

    IBM Cloud Baseline

--- main.tf
+++ main.tf
@@ -1,13 +1,56 @@
-provider "ibm" {
-  generation         = 2
-  region             = "au-syd"
+## Template to be used by the IBM Provider for Power Systems
+
+# Only needed if you need to execute a script within the VM
+# at initial VM deployment time
+#
+# resource "local_file" "vm_private_key" {
+#     content_base64    = var.vm_private_key_base64
+#     filename          = "tmp/id_rsa"
+# }
+
+data "ibm_pi_network" "power_networks" {
+    count                = length(var.networks)
+    pi_network_name      = var.networks
+    pi_cloud_instance_id = var.power_instance_id
 }
 
-data "ibm_iam_access_group" "accgroup" {
+data "ibm_pi_image" "power_images" {
+    pi_image_name        = var.image_name
+    pi_cloud_instance_id = var.power_instance_id
 }
 
-output "accgroups" {
-  value = data.ibm_iam_access_group.accgroup.groups[*].name
-}
+resource "ibm_pi_instance" "pvminstance" {
+    pi_memory               = var.memory
+    pi_processors           = var.processors
+    pi_instance_name        = var.vm_name
+    pi_proc_type            = var.proc_type
+    pi_storage_type         = var.storage_type
+    pi_migratable           = var.migratable
+    pi_image_id             = data.ibm_pi_image.power_images.id
+    pi_volume_ids           = []
+    pi_network { network_id = data.ibm_pi_network.power_networks.0.id }
+    pi_key_pair_name        = var.ssh_key_name
+    pi_sys_type             = var.system_type
+    pi_replication_policy   = var.replication_policy
+    pi_replication_scheme   = var.replication_scheme
+    pi_replicants           = var.replicants
+    pi_cloud_instance_id    = var.power_instance_id
 
-#This is a test comment
+#    Only needed if you need to execute a script within the VM
+#    at initial VM deployment time
+#
+#    provisioner "remote-exec" {
+#        scripts = [
+#            "scripts/wait_for_vm.sh",
+#            "scripts/bootstrap-aix.sh",
+#        ]
+#
+#        connection {
+#            type        = "ssh"
+#            host        = "${lookup(ibm_pi_instance.pvminstance.pi_network[0], "external_ip")}"
+#            timeout     = "15m"
+#            user        = "root"
+#            private_key = "${file("${local_file.vm_private_key.filename}")}"
+#        }
+#    }
+}
